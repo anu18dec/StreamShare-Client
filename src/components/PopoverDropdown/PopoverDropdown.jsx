43db143a6c365af54dd2cae322471e5228a5ea9d
Anupam Patel <dec18anu@gmail.com>
2025-01-11 12:57:39 +0530

    Fix component import error

--- src/components/PopoverDropdown/PopoverDropdown.jsx
+++ src/components/PopoverDropdown/PopoverDropdown.jsx
@@ -1,10 +1,10 @@
 import { useRef } from "react";
-import useOnClickOutside from "../../Hooks/useOnClickOutside.jsx";
+import UseOnClickOutside from "../../Hooks/useOnClickOutside.jsx";
 
 function PopoverDropdown({ children, dropdownOpen, setdropdownOpen }) {
     const ref = useRef();
 
-    useOnClickOutside(ref, (e) => {
+    UseOnClickOutside(ref, (e) => {
         if (dropdownOpen) setdropdownOpen((prev) => !prev);
     });
 
